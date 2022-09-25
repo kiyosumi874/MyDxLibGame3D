1f56f96bd6d3f1efadc5964fe1f1c79f7f89899a
kiyosumi874 <shiiharakiyosumi@gmail.com>
2022-09-25 17:53:27 +0900

    utility.hにエラー用のマクロを追加しました

--- src/Utility.h
+++ src/Utility.h
@@ -3,6 +3,10 @@
 // 2022 Kiyosumi Shiihara All Rights Reserved.
 //-----------------------------------------------------------------------------
 #pragma once
+#include <Windows.h>
 
 // 安全にインスタンスを開放
 #define SAFE_DELETE(p) { if(p) { delete p; p = nullptr; } }
+
+// エラーウィンドウ
+#define ERROR_MSG(str) { MessageBox(NULL, TEXT(str), TEXT("Error"),MB_OK | MB_ICONERROR); }
