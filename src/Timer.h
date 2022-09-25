7cd28974f0faeb4f2fb475c683749d0c2d9f043e
kiyosumi874 <shiiharakiyosumi@gmail.com>
2022-09-25 19:30:13 +0900

    Timerクラス実装

---
+++ src/Timer.h
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------------
+// @brief  Timerクラス
+// 2022 Kiyosumi Shiihara All Rights Reserved.
+//-----------------------------------------------------------------------------
+#pragma once
+#include <chrono>
+
+using namespace std::chrono;
+
+class Timer
+{
+public:
+	Timer(); // コンストラクタ.
+	~Timer(); // デストラクタ.
+
+	void Start(); // 計測開始.
+
+	const long long Elapsed() const; // 計測開始からの秒数をミリ秒で返す.
+	
+private:
+	system_clock::time_point startTime; // 計測開始時間.
+};
